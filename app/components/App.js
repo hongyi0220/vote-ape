d882b19e8450131836a9eab53e92afde46dd3757
Yungil Hong <hongyi0220@Yungils-MacBook.attlocal.net>
2017-11-28 19:55:05 -0600

    Add files

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                <h1>Vote Ape!!</h1>
+            </div>
+        );
+    }
+}
+
+module.exports = App;
