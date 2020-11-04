7fe90e7f0b5db7aa8d52d4dd24f56f9e0b8a2b3f
louisyungkang <hongyi0220@gmail.com>
2020-11-04 16:03:50 -0600

    Edit footer and its link

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,7 @@ export const Footer = props => {
     return (
         <div className='footer'>
             <footer>
-                About Me: <a href='http://yungilhong.com'>Yungil Hong</a> <br/>
+                About Me: <a href='http:/hongyi0220.github.io'>Louis Kang</a> <br/>
                 About This App: Inspiration from <a href='https://www.surveymonkey.com/'>SurveyMonkey</a>
             </footer>
             <div className='copyleft'>Copyleft © 2017-2049 PollMonkey</div>
